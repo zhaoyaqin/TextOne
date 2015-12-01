c94de31cb65a4521e2db0f21c322cbcaf6ef45f1
qin <18716001004@126.com>
2015-12-01 09:12:34 +0800

    add status

---
+++ Status/Status/Student.h
@@ -0,0 +1,15 @@
+//
+//  Student.h
+//  Status
+//
+//  Created by 赵亚琴 on 15/11/30.
+//  Copyright © 2015年 赵亚琴. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Student : NSObject
+
+@property (nonatomic, copy) NSString *name;
+
+@end
---
+++ Status/Status/Student.m
@@ -0,0 +1,13 @@
+//
+//  Student.m
+//  Status
+//
+//  Created by 赵亚琴 on 15/11/30.
+//  Copyright © 2015年 赵亚琴. All rights reserved.
+//
+
+#import "Student.h"
+
+@implementation Student
+
+@end
